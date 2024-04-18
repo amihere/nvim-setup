f8078cc947d22ec444863157f010a83a226b2b85
kwaw <kamihere@fandm.edu>
2024-04-18 21:20:13 +0000

    remap

--- lua/kyoto/core/keymaps.lua
+++ lua/kyoto/core/keymaps.lua
@@ -27,12 +27,12 @@ keymap.set("n", "<leader>w", "<C-w>w") -- toggle between open windows
 ----------------------
 
 -- telescope
-keymap.set("n", "<leader>cf", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>cG", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>cg", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>cb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>ch", "<cmd>Telescope help_tags<cr>") -- list available help tags
-keymap.set("n", "<leader>cF", "<cmd>Telescope git_files<cr>") -- git file search
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fG", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fg", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>fF", "<cmd>Telescope git_files<cr>") -- git file search
 
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
 
@@ -60,7 +60,7 @@ keymap.set("n", "<leader>.", "<cmd>lua require('harpoon.ui').nav_next()<cr>")
 keymap.set("n", "<leader>,", "<cmd>lua require('harpoon.ui').nav_prev()<cr>")
 
 -- new wave
-vim.keymap.set("n", "<leader>cv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
 
 -- moving lines up and down
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -85,14 +85,13 @@ keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 keymap.set("n", "<leader>Y", [["+Y]])
 keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-keymap.set({ "n", "v" }, "<leader>p", [["*p]])
-
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !~/.local/bin/tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("x", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>zz")
