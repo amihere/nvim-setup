81437f7f8b5449bdbe9f103c8eeed2e912a3a67d
kwaw <kamihere@fandm.edu>
2024-04-21 00:57:12 +0000

    add listeners for dapui to load on debug

--- lua/kyoto/plugins/dap.lua
+++ lua/kyoto/plugins/dap.lua
@@ -1,5 +1,20 @@
-require("dapui").setup()
+local dap, dapui = require("dap"), require("dapui")
+
+dapui.setup()
 require("nvim-dap-virtual-text").setup()
+
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
 
 vim.fn.sign_define(
 	"DapBreakpoint",
