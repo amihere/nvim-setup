63ac5c5e588b168276c5e9717bbc75c8d157b55c
kwaw <kamihere@fandm.edu>
2024-05-03 16:10:56 +0000

    lsp optimized

--- lua/kyoto/plugins/lsp/lspconfig.lua
+++ lua/kyoto/plugins/lsp/lspconfig.lua
@@ -14,7 +14,11 @@ end
 local on_attach = require("kyoto.plugins.lsp.keybinds").on_attach
 
 -- used to enable autocompletion (assign to every lsp server config)
-local capabilities = cmp_nvim_lsp.default_capabilities()
+local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_nvim_lsp.default_capabilities())
 
 -- Change the Diagnostic symbols in the sign column (gutter)
 -- (not in youtube nvim video)
