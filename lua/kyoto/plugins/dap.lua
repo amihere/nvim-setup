573add5e652edce6ebd1b9b4926394668c25b19a
kwaw <kamihere@fandm.edu>
2024-05-08 21:15:01 +0000

    use other bindings to walk through debug

--- lua/kyoto/plugins/dap.lua
+++ lua/kyoto/plugins/dap.lua
@@ -7,3 +7,12 @@ vim.api.nvim_set_keymap("n", "<leader>dt", ":DapTerminate<CR>", { desc = "Termin
 vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { desc = "Toggle breakpoint", noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { desc = "Start/Resume Debugger", noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>dr", ":DapToggleRepl<CR>", { desc = "Toggle Repl", noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dj", ":DapStepOver<CR>", { desc = "Step Over Code", noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dl", ":DapStepInto<CR>", { desc = "Step In Code", noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dh", ":DapStepOut<CR>", { desc = "Step Out Of Code", noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>dv",
+	":DapVirtualTextToggle<CR>",
+	{ desc = "Toggle Virtual Text", noremap = true }
+)
