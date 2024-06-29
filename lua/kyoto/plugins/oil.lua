e71a668ee752c17ce9b3a7746a5b8f0d98e160c6
kwaw <17128203+amihere@users.noreply.github.com>
2024-06-29 14:03:48 +0000

    configure oil with extra column

--- lua/kyoto/plugins/oil.lua
+++ lua/kyoto/plugins/oil.lua
@@ -1,1 +1,12 @@
-require("oil").setup()
+require("oil").setup({
+	view_options = {
+		natural_order = false,
+		is_always_hidden = function(name, bufnr)
+			vim.startswith(name, "..")
+		end,
+	},
+
+	columns = {
+		"size",
+	},
+})
