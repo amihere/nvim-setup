6cbf2fd3b1c1e3b379a17f492d734b69f1284415
gandalf <17128203+amihere@users.noreply.github.com>
2025-03-04 20:58:55 +0000

    Update plugins-setup.lua

--- lua/kyoto/plugins-setup.lua
+++ lua/kyoto/plugins-setup.lua
@@ -53,14 +53,9 @@ return packer.startup(function(use)
 	use("nvim-lualine/lualine.nvim")
 
 	-- fuzzy finding w/ telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }, -- dependency for better sorting performance
-		},
-	})
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { { "nvim-lua/plenary.nvim" } } }) -- fuzzy finder
+
 
 	-- autocompletion
 	use("hrsh7th/nvim-cmp") -- completion plugin
