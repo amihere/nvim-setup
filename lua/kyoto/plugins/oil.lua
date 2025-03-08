7d8db35ecaf5641de44c95df6f05089d6dbca502
gandalf <17128203+amihere@users.noreply.github.com>
2025-03-08 21:56:30 +0000

    Include gd for showing file permissions in oil

--- lua/kyoto/plugins/oil.lua
+++ lua/kyoto/plugins/oil.lua
@@ -5,6 +5,19 @@ require("oil").setup({
 			vim.startswith(name, "..")
 		end,
 	},
+	keymaps = {
+		["gd"] = {
+			desc = "Toggle file detail view",
+			callback = function()
+				detail = not detail
+				if detail then
+					require("oil").set_columns({ "permissions", "size", "mtime" })
+				else
+					require("oil").set_columns({})
+				end
+			end,
+		},
+	},
 
 	columns = {
 		"size",
