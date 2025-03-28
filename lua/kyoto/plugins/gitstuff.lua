9f604e194feab4d0740152f3bfad4fe8c30858f4
gandalf <17128203+amihere@users.noreply.github.com>
2025-03-28 16:18:38 +0000

    Update gitstuff.lua

--- lua/kyoto/plugins/gitstuff.lua
+++ lua/kyoto/plugins/gitstuff.lua
@@ -48,9 +48,7 @@ _gitsigns.setup({
 		map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "preview hunk" })
 		map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "preview hunk inline" })
 
-		map("n", "<leader>hb", function()
-			gitsigns.blame_line({ full = true })
-		end, { desc = "blame line (full)" })
+				map("n", "<leader>hB", gitsigns.blame, { desc = "blame line (full)" })
 
 		map("n", "<leader>hdd", gitsigns.diffthis, { desc = "diff against index" })
 		map("n", "<leader>hdb", ":Gitsigns change_base ", { desc = "change base to another branch/commit" })
@@ -61,18 +59,61 @@ _gitsigns.setup({
 		end, { desc = "diff against last commit" })
 
 		map("n", "<leader>hQ", function()
-			gitsigns.setqflist("all")
+			gitsigns.setqflist("all", nil)
 		end, { desc = "open all hunks in qflist" })
 		map("n", "<leader>hq", gitsigns.setqflist, { desc = "open hunks in buffer inside qflist" })
 
 		-- Toggles
-		map("n", "<leader>hB", gitsigns.toggle_current_line_blame, { desc = "toggle line blame" })
+		map("n", "<leader>hb", gitsigns.toggle_current_line_blame, { desc = "toggle line blame" })
 		map("n", "<leader>hw", gitsigns.toggle_word_diff, { desc = "toggle word diff" })
 
 		-- Text object
 		map({ "o", "x" }, "ih", gitsigns.select_hunk)
+
+		local function get_branch(callback)
+			local telescope = require("telescope.builtin")
+			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
+			telescope.git_branches({
+				attach_mappings = function(prompt_bufnr)
+					actions.select_default:replace(function()
+						-- Get the selected branch
+						local selection = action_state.get_selected_entry()
+
+						if selection then
+							-- Close Telescope window
+							actions.close(prompt_bufnr)
+
+							-- Extract branch name (remove asterisk and whitespace)
+							local branch_name = selection.value:gsub("^*%s*", "")
+
+							-- Call the provided callback with the branch name
+							if callback and type(callback) == "function" then
+								callback(branch_name)
+							end
+						end
+					end)
+
+					return true
+				end,
+			})
+		end
+
+		local function review_pr(branch_name)
+			local gs = require("gitsigns")
+			gs.change_base(branch_name, true, function()
+				gs.blame()
+				gs.setqflist("all", nil)
+				print("Opening all hunks from branch: " .. branch_name)
+			end)
+		end
+
+		vim.api.nvim_create_user_command("ReviewPR", function()
+			get_branch(review_pr)
+		end, {
+			desc = "Open quickfix list with hunks diffed against selected branch",
+		})
 	end,
 })
 
 require("git-worktree").setup()
-
