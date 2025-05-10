46e4e8b00b6e7572249c27e28344cc12d7fb2d81
kwaw <17128203+amihere@users.noreply.github.com>
2025-05-10 14:35:47 +0000

    include lsp config

--- lua/kyoto/plugins/lsp/mason.lua
+++ lua/kyoto/plugins/lsp/mason.lua
@@ -4,6 +4,34 @@ if not mason_status then
 	return
 end
 
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+	return
+end
+
 -- enable mason
 mason.setup()
 
+mason_lspconfig.setup({
+	-- list of servers for mason to install
+	ensure_installed = {
+		-- Lua
+		"lua_ls",
+		-- Elixir
+		"elixirls",
+		-- Python
+		"pylsp",
+		-- Front end
+		"biome",
+		"svelte",
+		"cssls",
+		"tailwindcss",
+		"html",
+		-- Java
+		"jdtls",
+		-- Go
+		"gopls",
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
+})
