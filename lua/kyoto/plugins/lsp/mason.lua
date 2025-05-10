9cc82dd648b896ff182ca819070b2113eae0f294
gandalf <17128203+amihere@users.noreply.github.com>
2025-05-10 14:37:07 +0000

    Crash (#38)
    * remove ruff, eslint, shrink mason file logic
    
    * add lemminx for xml formatting
    
    * fix jdtls issues
    
    * formatting
    
    * include lsp config

--- lua/kyoto/plugins/lsp/mason.lua
+++ lua/kyoto/plugins/lsp/mason.lua
@@ -4,15 +4,8 @@ if not mason_status then
 	return
 end
 
--- import mason-lspconfig plugin safely
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-	return
-end
-
--- import mason-null-ls plugin safely
-local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
-if not mason_null_ls_status then
 	return
 end
 
@@ -41,28 +34,4 @@ mason_lspconfig.setup({
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
-})
-
-mason_null_ls.setup({
-	-- list of formatters & linters for mason to install
-	ensure_installed = {
-		-- Lua
-		"stylua",
-		-- C
-		"clang_format",
-		"ruff",
-		"prettier",
-		"eslint_d",
-		-- Go
-		"gofumpt",
-		"goimports",
-		"gomodifytags",
-		"golangci-lint",
-		"gotests",
-		"iferr",
-		"impl",
-		"delve",
-	},
-	-- auto-install configured formatters & linters (with null-ls)
-	automatic_installation = true,
 })
