11160f20c9e4953b65d12d628c5646de8cb0428f
kwaw <17128203+amihere@users.noreply.github.com>
2025-10-12 22:57:49 +0000

    update signs

--- lua/kyoto/plugins/lsp/lspconfig.lua
+++ lua/kyoto/plugins/lsp/lspconfig.lua
@@ -16,10 +16,12 @@ local signs = { Error = " ", Warn = " ", Hint = "󱐋", Info = " " }
 
 vim.diagnostic.config({
 	signs = {
-		[vim.diagnostic.severity.ERROR] = signs.Error,
-		[vim.diagnostic.severity.WARN] = signs.Warn,
-		[vim.diagnostic.severity.HINT] = signs.Hint,
-		[vim.diagnostic.severity.INFO] = signs.Info,
+		text = {
+			[vim.diagnostic.severity.ERROR] = signs.Error,
+			[vim.diagnostic.severity.WARN] = signs.Warn,
+			[vim.diagnostic.severity.HINT] = signs.Hint,
+			[vim.diagnostic.severity.INFO] = signs.Info,
+		},
 	},
 })
 
