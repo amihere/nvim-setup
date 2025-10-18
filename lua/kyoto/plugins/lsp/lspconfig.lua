f2e4339515619cbd2ec6a2bc8fe37588576a262a
kwaw <17128203+amihere@users.noreply.github.com>
2025-10-18 02:50:30 +0000

    fix svelte server

--- lua/kyoto/plugins/lsp/lspconfig.lua
+++ lua/kyoto/plugins/lsp/lspconfig.lua
@@ -116,19 +116,28 @@ vim.lsp.config("tailwindcss", {
 	filetypes = { "typescript", "javascript", "svelte", "html", "css" },
 })
 
+vim.lsp.enable("ts_ls")
+
 -- configure svelte server
 vim.lsp.config("svelte", {
 	capabilities = capabilities,
-	filetypes = { "typescript", "javascript", "svelte", "html", "css" },
+	filetypes = { "svelte" },
 	on_attach = function(client, bufnr)
 		vim.api.nvim_create_autocmd("BufWritePost", {
 			pattern = { "*.js", "*.ts" },
+			group = vim.api.nvim_create_augroup("lspconfig.svelte", {}),
 			callback = function(ctx)
-				if client.name == "svelte" then
-					client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-				end
+				-- internal API to sync changes that have not yet been saved to the file system
+				client:notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
 			end,
 		})
+		vim.api.nvim_buf_create_user_command(bufnr, "LspMigrateToSvelte5", function()
+			client:exec_cmd({
+				title = "Migrate Component to Svelte 5 Syntax",
+				command = "migrate_to_svelte_5",
+				arguments = { vim.uri_from_bufnr(bufnr) },
+			})
+		end, { desc = "Migrate Component to Svelte 5 Syntax" })
 	end,
 })
 
