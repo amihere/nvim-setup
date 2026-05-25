f52212434e5516b7c644bdb127307676435cc555
kwaw <17128203+amihere@users.noreply.github.com>
2026-05-25 19:02:08 +0000

    add proper elixir ls command

--- lua/kyoto/plugins/lsp/lspconfig.lua
+++ lua/kyoto/plugins/lsp/lspconfig.lua
@@ -168,16 +168,17 @@ vim.lsp.config("html", {
 
 -- Configure ElixirLS as the LSP server for Elixir.
 vim.lsp.config("elixirls", {
-	cmd = { "/usr/local/opt/elixir-ls/libexec/language_server.sh" },
 	on_attach = on_attach, -- this may be required for extended functionalities of the LSP
 	capabilities = capabilities,
 	flags = {
 		debounce_text_changes = 150,
 	},
-	elixirLS = {
-		dialyzerEnabled = false,
-		fetchDeps = false,
-		projectDir = ".",
+	settings = {
+		elixirLS = {
+			dialyzerEnabled = false,
+			fetchDeps = false,
+			projectDir = ".",
+		},
 	},
 })
 
